b03551586c5c277c2184e8e80a1504e003d9292a
leah karienye <karienyeleah@gmail.com>
2022-06-05 00:30:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+function newHeader(rootNode) {
+    if ('textContent' in document.createTextNode(''))
+      return rootNode.textContent;
+  
+    var childNodes = rootNode.childNodes,
+        len = childNodes.length,
+        result = '';
+    
+    for (var i = 0; i < len; i++) {
+      if (childNodes[i].nodeType === 3)
+        result += childNodes[i].nodeValue;
+      else if (childNodes[i].nodeType === 1) 
+        result += textContent(childNodes[i]);
+    }
+  
+    return result;
+  }
